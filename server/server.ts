5606a5d4620bc5dce3ef60ca4b465767dd551d13
Deepshika Singh <deepshikasingh110@gmail.com>
2026-02-17 21:53:06 +0530

    Frontend ready for production

--- server/server.ts
+++ server/server.ts
@@ -16,7 +16,7 @@ const startServer = async () => {
 
   // ✅ CORS (allow local dev frontends)
   app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:3000'],
+  origin: ['http://localhost:5173', 'https://thumbnail-generator-33oq.onrender.com/'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
