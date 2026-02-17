28a0c8207f57457c687feaf5a38e890e4a184a93
Deepshika Singh <deepshikasingh110@gmail.com>
2026-02-17 22:36:20 +0530

    update origin

--- server/server.ts
+++ server/server.ts
@@ -16,7 +16,9 @@ const startServer = async () => {
 
   // ✅ CORS (allow local dev frontends)
   app.use(cors({
-  origin: ['http://localhost:5173', 'https://thumbnail-generator-33oq.onrender.com/'],
+  origin: ['http://localhost:5173', 'http://localhost:3000',
+    'https://thumbnailgenerator-eight.vercel.app/'
+  ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
